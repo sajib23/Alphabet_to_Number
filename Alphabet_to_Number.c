6f01c3c1d781af93509a468a29747cf553cf3385
Rasidul Islam Sajib <119812106+sajib23@users.noreply.github.com>
2023-02-26 01:13:27 +0600

    Add files via upload

---
+++ Alphabet_to_Number.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+int main(){
+char alphabet;
+printf("enter  alphabet you want to see it's number:\n");
+scanf("%s",&alphabet);
+switch (alphabet)
+{
+
+case 'a': printf("1\n");
+        break;
+
+case 'b': printf("2\n");
+        break;
+case 'c': printf("3\n");
+        break;
+case 'd': printf("4\n");
+        break;
+case 'e': printf("5\n");
+        break;
+case 'f': printf("6\n");
+        break;
+case 'g': printf("7\n");
+        break;
+case 'h': printf("8\n");
+        break;
+case 'i': printf("9\n");
+        break;
+case 'j': printf("10\n");
+        break;
+case 'k': printf("11\n");
+        break;
+case 'l': printf("12\n");
+        break;
+case 'm': printf("13\n");
+        break;
+case 'n': printf("14\n");
+        break;
+case 'o': printf("15\n");
+        break;
+case 'p': printf("16\n");
+        break;
+case 'q': printf("17\n");
+        break;
+case 'r': printf("18\n");
+        break;
+case 's': printf("19\n");
+        break;
+case 't': printf("20\n");
+        break;
+case 'u': printf("21\n");
+        break;
+case 'v': printf("22\n");
+        break;
+case 'w': printf("23\n");
+        break;
+case 'x': printf("24\n");
+        break;
+case 'y': printf("25\n");
+        break;
+case 'z': printf("26\n");
+        break;
+default :printf("invalid input\n");
+
+}
+
+    return 0;
+}
